70cfad8e406cfa049fa68281b4b7f673053845f9
Vitor Forbrig <vitorforbrig@gmail.com>
2026-02-03 13:13:03 -0300

    Improves layout with shared components

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,14 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 import { HomePage } from './pages/HomePage';
 import { GoogleServiceEncryptPage } from './pages/GoogleServiceEncryptPage';
+import { useConfig } from './hooks/useConfig';
 
 export function Router() {
+  const { config } = useConfig();
+  const baseUrl = config?.baseUrl || window.location.origin;
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/google-service-encrypt" element={<GoogleServiceEncryptPage />} />
-      </Routes>
+      <div className="min-h-screen bg-gray-100 flex flex-col">
+        <Header />
+        <main className="flex-1 p-6">
+          <div className="max-w-4xl mx-auto">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/google-service-encrypt" element={<GoogleServiceEncryptPage />} />
+            </Routes>
+          </div>
+        </main>
+        <Footer baseUrl={baseUrl} />
+      </div>
     </BrowserRouter>
   );
 }
